374289f24f2490afa7b2a40fea119fd8f578950d
lhrkhalil98 <lhrkhalil98@gmail.com>
2022-12-08 23:21:12 +0100

    first

---
+++ app/src/main/java/com/example/bicycleshop/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.bicycleshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME =2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Index.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                finish();
+            }
+        },2000) ;
+    }
+}
